fa380a75dc2431afddf1f4c665aaa49291e39aa0
Anthony Valle <valleganthony@gmail.com>
2019-04-29 14:55:38 -0400

    Updated account class

--- src/com/company/ATM.java
+++ src/com/company/ATM.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class ATM {
+    private double deposit;
+    private double withdraw;
+    private double checkBalance;
+    private  double balance;
+
+
+
 }
--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class Account {
+
+    private int accountNumber;
+    private String userName;
+    private ATM balance ="some balance";
+    private int pin;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public Account() {
+    }
+
+    public Account(int accountNumber, String userName, int pin) {
+        this.accountNumber = accountNumber;
+        this.userName = userName;
+        this.pin = pin;
+    }
+
+
+
+
 }
