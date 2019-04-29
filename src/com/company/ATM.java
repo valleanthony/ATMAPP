e2841770182174bf79d961f6f9c747eb081f4f74
fikrua <fikruayal@gmail.com>
2019-04-29 15:56:23 -0400

    UPDATED ATM CLASS

--- src/com/company/ATM.java
+++ src/com/company/ATM.java
@@ -3,16 +3,29 @@ package com.company;
 import java.util.Scanner;
 
 public class ATM {
-    Account FIKRU = new  Account();
+    Account FIKRU = new  Account("fi","12ddf",700,"100");
 
 
     private  double deposite;
     private double withdraw;
-    private double balance;
+    private double balance = FIKRU.getBalance();
     Scanner scan = new Scanner(System.in);
     public void userinfo(){
-        System.out.println("");
+        System.out.println("enter your name ");
+        String name = scan.nextLine();
+        System.out.println("enter urpin no ");
+        String pinNo = scan.nextLine();
+        System.out.println("do you want deposit or withdraw");
+        String withdrwaDeposit = scan.nextLine();
+        if(withdrwaDeposit.equalsIgnoreCase("withdraw"))
+            setWithdraw();
+        else if(withdrwaDeposit.equalsIgnoreCase("deposit"))
+            setDeposite();
 
+
+    }
+
+    public ATM() {
     }
 
     public ATM(double deposite, double withdraw) {
@@ -35,16 +48,23 @@ public class ATM {
     public void setWithdraw(double withdraw) {
         this.withdraw = withdraw;
     }
+    public double getBalance(){
+        return balance;
+    }
 
     public void setDeposite(){
-        if(this.balance<withdraw)
+        if(this.balance<this.withdraw)
             System.out.println("you don't have sufficient balance ");
-        this.balance = this.balance-getWithdraw();
+        else
+            this.balance = this.balance+this.deposite;
+
+        System.out.println( "you deposite "+ this.withdraw +" now the balance is " + this.balance);
 
 
     }
     public void setWithdraw(){
-        this.balance = this.balance+this.deposite;
+        this.balance = this.balance-this.withdraw;
+        System.out.println( "you withdraw "+ this.withdraw +" now the balance is " + this.balance);
 
     }
 
